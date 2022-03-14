035dffe2938b618954b5be5c493f6c5397a7b03b
root <balecrazium@gmail.com>
2022-03-14 14:01:32 -0700

    adding numberz

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 
 /**
- * main -print all possible diff combination of three digits
- * return: always 0 (Success)
+ *   main - prints all possible different combinations of three digits
+ *   Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n, m , l;
-	for (n = 48; n < 58; n++)
-	{
-		for (m = 49; m < 58; m++)
+		int n, m, l;
+		for (n = 48; n < 58; n++)
 		{
-			for (1 = 50; 1 < 58; 1++)
+			for (m = 49; m < 58; m++)
 			{
-				if (1 > m && m > n)
+				for (l = 50; l < 58; m++)
 				{
-					putchar(n);
-					putchar(m);
-					putchar(1);
-					if (n != 55 || m != 56)
+					if (l > m && m > n)
 					{
-						putchar(',');
-						putchar(',');
+						putchar(n);
+						putchar(m);
+						putchar(1);
+						if (n != 55 || m != 56)
+						{
+							putchar(',');
+							putchar(' ');
+						}
 					}
 				}
 			}
 		}
-	}
-	putchar('\n');
-	return(0);
+		putchar('\n');
+		return (0);
+}
