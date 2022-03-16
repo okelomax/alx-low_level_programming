1a6691369b8bfdcbfc36d5ed6268e638e542d0b1
root <balecrazium@gmail.com>
2022-03-16 05:41:12 -0700

    added task 1 of 11 to project 0x02 c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+include "stdio.h"
+/**
+ * print_alphabet - print the alphabet, in lowercase, followed by a new line
+ */
+void print_alphabet(void)
+{
+	char ch;
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+}
