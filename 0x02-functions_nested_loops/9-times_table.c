5171e9541c21095eca2783061c8dad161407327b
root <balecrazium@gmail.com>
2022-03-16 17:42:16 -0700

    added task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,11 +26,10 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((k / 10) = '0');
+				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
 	}
 }
-
