ffcfcbeed4b42f717fb8a765b24508570a6a2d9d
root <balecrazium@gmail.com>
2022-03-18 05:51:07 -0700

    added task 1 to 0x03

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+
+	int i;
+	printf("infinite loop incoming :(\n");
+	
+	/*
+	 * 1 = 0'
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+
+	printf("infinite loop avoide! \\o/\n");
+
+	return (0);
+}
